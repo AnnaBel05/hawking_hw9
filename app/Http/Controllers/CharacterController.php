70123c6a6a4f8f9c6af478d580227ae7e4efee6c
AnnaBel05 <anebuchenkova@yandex.ru>
2022-08-01 18:40:34 +0300

    hawking hw19

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -7,6 +7,8 @@ use App\Models\Character;
 use App\Http\Requests\StorePostRequest;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class CharacterController extends Controller
 {
@@ -34,7 +36,22 @@ class CharacterController extends Controller
     // todo:
     public function show($id)
     {
-        $character = Character::where('id',1);
+        $character = Character::where('id',$id)->get();
+
+        if (Storage::disk('public')->exists($character->image_link))
+        {
+            $contents = Storage::get($character->image_link);
+        }
+
+        $url = Storage::url($character->image_link);
+
+        $img = Image::make('test.jpg')
+            ->resize(200,200)
+            ->insert($character
+            ->image_link,'top-left')
+            ->text('testImage',0,0)
+        ;
+
         Cache::put('characterBest', $character);
         return view('show-character', compact('character'));
     }
@@ -56,12 +73,18 @@ class CharacterController extends Controller
     {
         $validated = $request->validated();
 
-        $img = $request->file('image_link')->store('images', 'public');
+        $img = $request->file('image_link')->store('characters', 'public');
         
         $character = new Character;
         $character->name = $validated['name'];
         $character->description = $request->description;
         $character->image_link = $img;
+
+        $image = Image::make(Storage::get($character->image_link));
+        $image->resize(100, 100);
+
+        Storage::put("$img", $image, 'public');        
+
         $character->save();
         Cache::put('characterRecent', $character);
         Cache::tags(['characters', 'latest'])->put('characterRecent', $character);
--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -212,6 +213,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Image' => Intervention\Image\Facades\Image::class,
     ])->toArray(),
 
 ];
